b75b4ab2912302dc78a08631dfe46084da50df7d
USER <cji513@naver.com>
2024-12-12 21:41:03 +0900

    feat(data.sql): 서버 실행 시 데이터 삽입
    로컬 테스트용

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into Member (main_id,login_id,login_password) VALUES (11,'chung','1234');
+
+insert into Member_info (main_id, name, exp, point, point_role, choose_role) values (11,'정종인',20,5,0,0);
+
+insert into mission (id, content, content_role) values (1,'방 청소하기',0);
+insert into mission (id, content, content_role) values (2,'밥 세끼 챙겨먹기',0);
+insert into mission (id, content, content_role) values (3,'책 한시간 읽기',0);
+insert into mission (id, content, content_role) values (4,'설거지 하기',0);
+insert into mission (id, content, content_role) values (5,'산책하기',1);
+insert into mission (id, content, content_role) values (6,'영화보기',1);
+insert into mission (id, content, content_role) values (7,'카페에서 음료수 사오기',1);
+insert into mission (id, content, content_role) values (8,'운동하기',1);
+insert into mission (id, content, content_role) values (9,'지인 만나기',2);
+insert into mission (id, content, content_role) values (10,'지인과 통화하기',2);
+insert into mission (id, content, content_role) values (11,'지인과 같이 운동하기',2);
+insert into mission (id, content, content_role) values (12,'지인과 카페가기',2);
